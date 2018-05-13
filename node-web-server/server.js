79c808a6cddac66df615049d51810b5c341ce8e1
chaomeng <chaomeng04@gmail.com>
2018-05-13 12:31:20 -0700

    render dynamic page

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,19 +1,23 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
+
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-	res.send({
-		Name: 'test',
-		like: [
-			'start work'
-		]
+	res.render('home.hbs', {
+		PageTitle: 'About Page',
+		WelcomeMessage: 'Welcome to Home Page'
 	});
 });
 
 app.get('/about', (req, res) => {
-	res.send('hello, world');
+	res.render('about.hbs', {
+		PageTitle: 'About Page',
+		CurrentYear: new Date().getFullYear()
+	});
 });
 
 app.listen(3000);
