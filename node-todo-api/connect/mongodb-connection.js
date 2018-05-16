06bba090fe3547b45ae59c95d4634173b5c53f78
chaomeng <chaomeng04@gmail.com>
2018-05-16 00:54:26 -0700

    mongodb find

--- node-todo-api/connect/mongodb-connection.js
+++ node-todo-api/connect/mongodb-connection.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
 	if (err) {
@@ -7,9 +8,10 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
 	console.log('Connect to MongoDB server.');
 	const db = client.db('TodoApp');
 
-	db.collection('Todos').insertOne({
-		text: 'something to do',
-		completed: false
+	db.collection('Users').insertOne({
+		name: 'Chao',
+		age: 29,
+		location: 'Alhambra'
 	}, (err, result) => {
 		if (err) {
 			console.log('Unable to insert row', err);
