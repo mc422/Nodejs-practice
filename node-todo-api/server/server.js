c3bb91f6fc5ff9dc2ed6aeb48c577d0b4f827862
chaomeng <chaomeng04@gmail.com>
2018-05-23 00:13:14 -0700

    redesign user model, and POST /users route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -87,9 +87,20 @@ app.patch('/todos/:id', (req, res) => {
     if (!todo) {
       return res.status(404).send();
     }
-    res.send({todo})
+    res.send({todo});
   }).catch((err) => {
     res.status(400).send();
+  });
+});
+
+app.post('/users', (req, res) => {
+  var payload = _.pick(req.body, ['email', 'password']);
+  var user = new User(payload);
+
+  user.save().then((user) => {
+    res.send(user);
+  }).catch((err) => {
+    res.status(400).send(err);
   });
 });
 
