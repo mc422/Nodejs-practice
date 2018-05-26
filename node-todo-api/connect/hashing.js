fb9e807fe04bcc00bcd9fec9ac78ef1475c3f50c
chaomeng <chaomeng04@gmail.com>
2018-05-26 13:27:31 -0700

    add mongoose middleware to hash password

--- node-todo-api/connect/hashing.js
+++ node-todo-api/connect/hashing.js
@@ -1,14 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var test = 'what a fucking life I have, fucking me, fucking you god';
-console.log(SHA256(test).toString());
 
-var data = {
-	id: 10
-};
+password = 'abc123';
 
-var token = jwt.sign(data, 'abc123');
+bcrypt.genSalt(10, (err, salt) => {
+	bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
 
-var decode = jwt.verify(token, 'abc123');
-console.log('decode ', decode);
+    bcrypt.compare(password, hash, (err, res) => {
+      console.log(`res: ${res}`);
+    });
+  });
+});
+
+
+
+
+// var test = 'what a fucking life I have, fucking me, fucking you god';
+// console.log(SHA256(test).toString());
+
+// var data = {
+// 	id: 10
+// };
+
+// var token = jwt.sign(data, 'abc123');
+
+// var decode = jwt.verify(token, 'abc123');
+// console.log('decode ', decode);
--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -1,7 +1,8 @@
+const _ = require('lodash');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
-const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var userSchema = new mongoose.Schema({
   email: {
@@ -71,7 +72,22 @@ userSchema.statics.findByToken = function (token) {
     'tokens.token': token,
     'tokens.access': access
   });
-}
+};
+
+userSchema.pre('save', function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash,
+        next();
+      })
+    })
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model('User', userSchema);
 
